ed5478161ee65adae0259344e8fe2d27ef3db3d0
ZahranHM <zhaykalm@gmail.com>
2023-07-23 19:28:03 +0700

    Add Scene PlayDummy - ZahranHM
    This is for the Host copying new feature from the dummy scene into main scene.
    - FinishArea
    - Timer (TimeManager and TimeController)
    - Also a GameManager

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TimeManager timeManager;
+    public Text minutesShow;
+    public Text secondsShow;
+    private int minutesNumber;
+    private int secondsNumber;
+
+    public void ShowTheNumbers()
     {
-        
+        minutesShow.text = minutesNumber.ToString();
+        secondsShow.text = secondsNumber.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public int MinutesNumber
     {
-        
+        get {return minutesNumber;}
+        set {minutesNumber = value;}
     }
+    public int SecondsNumber
+    {
+        get { return secondsNumber; }
+        set { secondsNumber = value; }
+    }
+
 }

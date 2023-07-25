d8ca5edd92fbfdefb86b0934e086d561cd80f9cb
Wisnu Eka <wisnuekasaputra@hotmail.com>
2023-07-26 06:15:16 +0700

    Update Scene Enemy
    Update logic enemy
    Add Minimum Search destination value
    Add Path Blocker, so the enemy can set destination to the area inside building
    Add visual gizmo destination point.
    
    For Next update :
    - Implement 3d Model and animation to enemy
    - Enemy looking right and left while patrol
    - TBD

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -22,6 +22,7 @@ public class EnemyAI : MonoBehaviour
     private EnemyState currentState;
 
     private Vector3 originalPosition; // Store the original position for patrolling back
+    private Vector3 lastKnownPlayerPosition; // Store the last known player position
 
     void Awake()
     {
@@ -29,6 +30,7 @@ public class EnemyAI : MonoBehaviour
         player = GameObject.FindWithTag("Player").transform; // Assuming the player has the "Player" tag.
         currentState = EnemyState.Patrol;
         originalPosition = transform.position;
+        lastKnownPlayerPosition = originalPosition;
     }
 
     void Update()
@@ -44,6 +46,7 @@ public class EnemyAI : MonoBehaviour
                 }
                 else
                 {
+                    lastKnownPlayerPosition = player.position; // Store the player's position when detected
                     currentState = EnemyState.Chase;
                 }
                 break;
@@ -51,6 +54,7 @@ public class EnemyAI : MonoBehaviour
             case EnemyState.Chase:
                 if (playerInSight)
                 {
+                    lastKnownPlayerPosition = player.position; // Update the last known position while chasing
                     Chase();
                 }
                 else
@@ -102,25 +106,42 @@ public class EnemyAI : MonoBehaviour
 
         if (isPatrolling)
         {
-            enemy.SetDestination(enemy.destination); // Continue moving towards the current destination.
+            // Check if the enemy has reached the destination
+            if (!enemy.pathPending && enemy.remainingDistance < 0.1f)
+            {
+                // Resume patrolling by starting a new search for the destination
+                StartCoroutine(SearchDestination());
+            }
         }
     }
 
     IEnumerator SearchDestination()
     {
         isPatrolling = true;
+        Vector3 finalPosition = Vector3.zero;
+        float minimumDistance = 10f; // Minimum distance for the patrol destination
 
-        // Generate a random position within the walk range
-        Vector3 randomDirection = Random.insideUnitSphere * walkRange;
-        randomDirection += transform.position;
-        NavMeshHit hit;
-        NavMesh.SamplePosition(randomDirection, out hit, walkRange, 1);
-        Vector3 finalPosition = hit.position;
-
-        // Ensure the position is on the NavMesh and reachable
-        if (NavMesh.Raycast(transform.position, finalPosition - transform.position, out NavMeshHit navHit, NavMesh.AllAreas))
+        // Generate a random position within the walk range, ensuring it meets the minimum distance requirement
+        while (true)
         {
-            finalPosition = navHit.position;
+            Vector3 randomDirection = Random.insideUnitSphere * walkRange + (transform.position - originalPosition).normalized * minimumDistance;
+
+            // Ensure the position is on the NavMesh and reachable
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomDirection, out hit, walkRange, 1))
+            {
+                finalPosition = hit.position;
+                break;
+            }
+
+            yield return null;
+        }
+
+        // Ensure the final position is on the NavMesh and reachable
+        NavMeshHit finalHit;
+        if (NavMesh.Raycast(transform.position, finalPosition - transform.position, out finalHit, NavMesh.AllAreas))
+        {
+            finalPosition = finalHit.position;
         }
 
         enemy.SetDestination(finalPosition);
@@ -142,10 +163,17 @@ public class EnemyAI : MonoBehaviour
     IEnumerator IdleBeforePatrol()
     {
         // Wait for a few seconds before going back to patrol
-        yield return new WaitForSeconds(2f); // You can adjust the idle time here
+        yield return new WaitForSeconds(1f); // You can adjust the idle time here
 
-        enemy.SetDestination(originalPosition);
-        currentState = EnemyState.Patrol;
+        enemy.SetDestination(lastKnownPlayerPosition); // Go to the last known player position
+        currentState = EnemyState.Chase; // Set to Chase state to continue chasing if the player comes back
+
+        while (Vector3.Distance(transform.position, lastKnownPlayerPosition) > 1f)
+        {
+            yield return null;
+        }
+
+        currentState = EnemyState.Patrol; // Change back to Patrol state after reaching the last known position
     }
 
     // Draw Gizmos for FOV angle and sight radius
@@ -162,5 +190,13 @@ public class EnemyAI : MonoBehaviour
         // Draw sight radius Gizmo (red sphere)
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, sightRange);
+
+        // Draw the destination gizmo (blue sphere)
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(enemy.destination, 0.3f);
+
+        // Draw a straight line from the enemy's current position to the destination
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position, enemy.destination);
     }
 }
